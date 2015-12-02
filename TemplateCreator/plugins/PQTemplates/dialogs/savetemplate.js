936776aeb3653aa30d811ede97fe4679d80858bc
tbrownfield <tbrownfield@gmail.com>
2015-12-02 13:31:54 -0700

    update

--- TemplateCreator/plugins/PQTemplates/dialogs/savetemplate.js
+++ TemplateCreator/plugins/PQTemplates/dialogs/savetemplate.js
@@ -3,84 +3,43 @@ CKEDITOR.dialog.add( 'PQSaveTemplateDialog', function(  ) {
         title: 'Save Template',
         minWidth: 300,
         minHeight: 150,
-        contents: [
-            {
-                id: 'tab1',
-                label: 'Only Tab',
-                elements: [
-                    {
-						
-						type: 'vbox',
-						widths: [ '25%', '25%', '25%', '25%' ],
-						children: [
-							{	
-								type: 'text',
-								id: 'tempname',
-								label: 'Name',
-								style: 'width:300px',
-								onShow: function() {
-									var tempname = decodeURIComponent(document.URL.match(/&temp=([^&]+)/))
-									if (tempname) {
-										var dialog = CKEDITOR.dialog.getCurrent()
-										dialog.setValueOf("tab1","tempname",tempname)
-									}
-								},
-								validate: CKEDITOR.dialog.validate.notEmpty( "Please enter a name for this template." )
-							},
-							{
-								type: 'checkbox',
-								id: 'noreply',
-								label: 'No Reply',
-								title: 'Use the \"No Reply\" footer'
-							},
-							{
-								type: 'text',
-								id: 'distros',
-								label: 'Default Distros',
-								title: 'Email addresses to populate to the To field for every email using this template.'
-							},
-							{
-								type: 'select',
-								id: 'subject',
-								label: 'Subject',
-								title: 'Default subject line for emails using this template.',
-								onLoad: function() {
-									var dbid = editor.config.PQTemplates.TemplateQB.dbid
-									var appToken = editor.config.PQTemplates.TemplateQB.appToken
-									var subjectFid = editor.config.PQTemplates.TemplateQB.subjectFid
-									
-									var url="";
-									url +="https://intuitcorp.quickbase.com/db/"+dbid;
-									url +="?act=API_AddRecord";
-
-									var request="";
-									request += '<qdbapi>';
-									request += '<apptoken>'+appToken+'</apptoken>';
-									request += '</qdbapi>';
-
-									jQuery.ajax({
-										type: "POST",
-										contentType: "text/xml",
-										url: url,
-										dataType: "xml",
-										processData: false,
-										data: request,
-										success: function(xml) {
-											var options = $("field #"+subjectFid+" choices",xml);
-											$.each(options, function() {
-												this.add($(this).text())
-											})
-										},
-										error: function() {
-										}
-									});
-								}
+        contents: [{
+			id: 'tab1',
+			label: 'Only Tab',
+			elements: [{
+				type: 'vbox',
+				widths: [ '25%', '25%', '25%', '25%' ],
+				children: [
+					{	
+						type: 'text',
+						id: 'tempname',
+						label: 'Name',
+						style: 'width:300px',
+						onShow: function() {
+							var tempname = decodeURIComponent(document.URL.match(/&temp=([^&]+)/))
+							if (tempname) {
+								var dialog = CKEDITOR.dialog.getCurrent()
+								dialog.setValueOf("tab1","tempname",tempname)
 							}
-						]
+						},
+						validate: CKEDITOR.dialog.validate.notEmpty( "Please enter a name for this template." )
+					},
+					{
+						type: 'checkbox',
+						id: 'noreply',
+						label: 'No Reply',
+						title: 'Use the \"No Reply\" footer'
+					},
+					{
+						type: 'text',
+						id: 'distros',
+						label: 'Default Distros',
+						title: 'Email addresses to populate to the To field for every email using this template.'
+
 					}
 				]
-			}
-		],
+			}]
+		}],
         onOk: function() {
             var dialog = this;
 			var editor = CKEDITOR.instances.editor;
