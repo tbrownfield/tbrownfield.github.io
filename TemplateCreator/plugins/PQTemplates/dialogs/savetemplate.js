5275fd971ce10644f83ee61be3156124353e8e12
tbrownfield <tbrownfield@gmail.com>
2015-12-02 13:16:46 -0700

    update

--- TemplateCreator/plugins/PQTemplates/dialogs/savetemplate.js
+++ TemplateCreator/plugins/PQTemplates/dialogs/savetemplate.js
@@ -18,63 +18,15 @@ CKEDITOR.dialog.add( 'PQSaveTemplateDialog', function(  ) {
 								id: 'tempname',
 								label: 'Name',
 								style: 'width:300px',
-								//onShow: function() {
-								//	var tempname = decodeURIComponent(document.URL.match(/&temp=([^&]+)/))
-								//	if (tempname) {
-								//		var dialog = CKEDITOR.dialog.getCurrent()
-								//		dialog.setValueOf("tab1","tempname",tempname)
-								//	}
-								//},
+								onShow: function() {
+									var tempname = decodeURIComponent(document.URL.match(/&temp=([^&]+)/))
+									if (tempname) {
+										var dialog = CKEDITOR.dialog.getCurrent()
+										dialog.setValueOf("tab1","tempname",tempname)
+									}
+								},
 								validate: CKEDITOR.dialog.validate.notEmpty( "Please enter a name for this template." )
-							},
-							{
-								type: 'checkbox',
-								id: 'noreply',
-								label: 'No Reply',
-								title: 'Use the \"No Reply\" footer',
-							},
-							{
-								type: 'textInput',
-								id: 'distros',
-								label: 'Default Distros',
-								title: 'Email addresses to populate to the To field for every email using this template.'
-							},
-							{
-								type: 'select',
-								id: 'subject',
-								label: 'Subject',
-								title: 'Default subject line for emails using this template.',
-								onLoad: function() {
-									var dbid = editor.config.PQTemplates.TemplateQB.dbid
-									var appToken = editor.config.PQTemplates.TemplateQB.appToken
-									var subjectFid = editor.config.PQTemplates.TemplateQB.subjectFid
-									
-									var url="";
-									url +="https://intuitcorp.quickbase.com/db/"+dbid;
-									url +="?act=API_AddRecord";
 
-									var request="";
-									request += '<qdbapi>';
-									request += '<apptoken>'+appToken+'</apptoken>';
-									request += '</qdbapi>';
-
-									jQuery.ajax({
-										type: "POST",
-										contentType: "text/xml",
-										url: url,
-										dataType: "xml",
-										processData: false,
-										data: request,
-										success: function(xml) {
-											var options = $("field #"+subjectFid+" choices",xml);
-											$.each(options, function() {
-												this.add($(this).text())
-											})
-										},
-										error: function() {
-										}
-									});
-								}
 							}
 						]
 					}
