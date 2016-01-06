b961357a87731d64d24bb7a7557fbb45fbd554d1
tbrownfield <tbrownfield@gmail.com>
2016-01-06 16:15:32 -0700

    update

--- CKEditor/plugins/helpdocs/plugin.js
+++ CKEditor/plugins/helpdocs/plugin.js
@@ -2,6 +2,7 @@ CKEDITOR.plugins.add( 'helpdocs', {
 	icons: 'helpdocs',
 	init: function()
 	{
+		var editor = CKEDITOR.instances.editor
 		editor.addCommand( 'helpdocs', {
 			exec: function () {
 				window.open('https://intuitcorp.quickbase.com/db/bkemhpu9e?a=dbpage&pageID=3','Help Docs','location=0,status=0,resizable=1,width=800,height=750');
