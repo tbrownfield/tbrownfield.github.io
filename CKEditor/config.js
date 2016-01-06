20202d7dad35c027868c447308f59d011a9ce793
tbrownfield <tbrownfield@gmail.com>
2016-01-06 16:11:32 -0700

    update

--- CKEditor/config.js
+++ CKEditor/config.js
@@ -8,7 +8,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// For complete reference see:
 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
 
-	config.extraPlugins = 'email,dropler,image2,PQTemplates',
+	config.extraPlugins = 'email,dropler,image2,PQTemplates,helpdocs',
 	
 	// The toolbar groups arrangement, optimized for two toolbar rows.
 	config.toolbarGroups = [
---
+++ CKEditor/plugins/helpdocs/plugin.js
@@ -0,0 +1,16 @@
+CKEDITOR.plugins.add( 'helpdocs', {
+	icons: 'helpdocs',
+	init: function()
+	{
+		editor.addCommand( 'helpdocs', {
+			exec: function () {
+				window.open('https://intuitcorp.quickbase.com/db/bkemhpu9e?a=dbpage&pageID=3','Help Docs','location=0,status=0,resizable=1,width=800,height=750');
+			}
+			
+		}
+		editor.ui.addButton( 'helpdocs', {
+			label: 'Help Docs',
+			command: 'helpdocs',
+			toolbar: 'help,0'
+		});
+	}
